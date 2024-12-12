fc4db1313f651813732c93f450f7cfa34985182f
MaheshworDhakal <maheshwordhakal24@gmail.com>
2024-12-12 16:59:51 +0545

    commit the files

---
+++ Sum of natural number.cpp
@@ -0,0 +1,24 @@
+// To find the sum of nth natural number
+#include<stdio.h>
+int main()
+{
+int i,n,sum=0;
+printf(" enter the number till you want to sum ");
+scanf("%d",&n);
+for(i=1;i<=n;i++)
+{sum= sum+i;
+
+}
+printf("the sum of number is %d\n",sum);	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	return 0;
+}
